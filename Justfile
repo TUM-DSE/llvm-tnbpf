7a22f67adfeded95c0f9444cb552a5b77d83d19c
Martin Fink <martin@finkmartin.com>
2026-04-21 11:27:05 +0000

    setup nix

---
+++ Justfile
@@ -0,0 +1,27 @@
+help:
+    just -l
+
+clean:
+    rm -rf build
+
+configure:
+    #!/usr/bin/env bash
+    mkdir -p build
+    cd build
+    cmake -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" \
+        -DLLVM_TARGETS_TO_BUILD="BPF" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DLLVM_ENABLE_ASSERTIONS="ON" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS="ON" \
+        -DLLVM_CCACHE_BUILD="ON" \
+        -DLLVM_ENABLE_LLD="OFF" \
+        -DLLVM_USE_LINKER="mold" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DLLVM_PARALLEL_LINK_JOBS=1 \
+        -DCMAKE_DISABLE_PRECOMPILE_HEADERS=ON \
+        -G "Ninja" \
+        ../llvm
+
+build:
+    ninja -Cbuild
---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Flake for LLVM development.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      unstable,
+      ...
+    }@inputs:
+    inputs.utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          unstablePkgs = import unstable {
+            inherit system;
+          };
+        in
+        {
+          devShell = pkgs.mkShell rec {
+            name = "llvm dev env";
+            hardeningDisable = [
+              "libcxxhardeningfast"
+              "strictflexarrays1"
+              "strictflexarrays3"
+            ];
+
+            packages = with pkgs; [
+              # Development Tools
+              ccache
+              gdb
+              cmake
+              ninja
+              graphviz
+              llvmPackages_latest.lldb
+              pkg-config
+              unstablePkgs.mold
+              clang
+              zlib
+              just
+            ];
+
+            NIX_LDFLAGS = "-rpath ${pkgs.zlib}/lib";
+          };
+        }
+      );
+}
